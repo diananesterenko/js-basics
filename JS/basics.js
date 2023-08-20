7bdf7beff9ba06c58b4d22a77bddbfb5ab1f51f0
Diana <dianasemak08@gmail.com>
2023-08-20 16:00:36 +0300

    Make jsBasics tasks

---
+++ JS/basics.js
@@ -0,0 +1,43 @@
+//---------------Змінні-----------------
+//1
+let a = 2;
+let b = 10;
+console.log(a * b);
+//2
+let c = 20;
+let d = 10;
+console.log(c / d);
+//3
+let e = 200;
+let f = 10;
+console.log(e + f);
+//4
+let number = 11;
+let bool = true;
+let str = "java script";
+let numStr = "100";
+console.log(number, bool, str, numStr);
+//5
+let num = 1;
+num += 11;
+num -= 11;
+num *= 11;
+num /= 11;
+num++;
+num--;
+//------------Prompt-------------
+//1
+let numb = prompt("Enter a number");
+numb **= 2;
+alert(numb);
+//2
+const num1 = prompt("Enter first number");
+const num2 = prompt("Enter second number");
+console.log((Number(num1) + Number(num2)) / 2);
+//3
+const minutes = prompt("Enter minutes:");
+console.log(Number(minutes) * 60);
+//4
+const greeting = "Hello";
+const userName = prompt("Enter your name: ");
+console.log(`${greeting}, ${userName} !`);
